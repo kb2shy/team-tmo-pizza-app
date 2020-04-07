911287c6804c09d67643fc6729d699283d6280cc
JadeMM <jade.moksness@gmail.com>
2020-04-07 15:55:01 -0700

    Bar for under toppings

---
+++ client/pizza-app/src/components/BarChart.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+export default class BarChart extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            height: 10,
+            width: 300,
+            data: {label: 'pepperoni', value: 0.4}
+        }
+    }
+
+    componentDidMount = () => {
+        this.drawCanvas()
+    }
+
+    drawCanvas = () => {
+        const {width, height} = this.state;
+
+        //canvas
+        const svg = d3.select('#chart')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g');
+        
+        //bar
+        svg.append("rect")
+            .attr('height', height)
+            .attr('width', width)
+            .style("fill", "gray");
+
+        //fill
+        svg.append("rect")
+            .attr('height', height)
+            .attr('width', width * this.state.data.value)
+            .style('fill', 'brown');
+
+    }
+
+    render() {
+        return <div id='bar'/>
+    }
+}
