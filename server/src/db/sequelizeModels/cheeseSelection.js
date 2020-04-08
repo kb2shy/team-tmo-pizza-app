51bcb1d3f718fb747d53434febe501a7c10ec121
unknown <amickey.rivera@gmail.com>
2020-04-08 11:43:23 -0700

    finished setting up db models and relations

--- server/src/db/sequelizeModels/cheeseSelection.js
+++ server/src/db/sequelizeModels/cheeseSelection.js
@@ -4,15 +4,6 @@ const sequelize = require('../dbConfig')
 class CheeseSelection extends Model{}
 
 CheeseSelection.init({
-    cheese_selection_id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    pizza_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
 }, {sequelize, modelName: 'cheeseSelection', timestamps: false})
 
 module.exports = CheeseSelection
--- server/src/db/sequelizeModels/meatSelection.js
+++ server/src/db/sequelizeModels/meatSelection.js
@@ -4,15 +4,7 @@ const sequelize = require('../dbConfig')
 class MeatSelection extends Model{}
 
 MeatSelection.init({
-    meat_selection_id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    pizza_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
+
 }, {sequelize, modelName: 'meatSelection', timestamps: false})
 
 module.exports = MeatSelection
