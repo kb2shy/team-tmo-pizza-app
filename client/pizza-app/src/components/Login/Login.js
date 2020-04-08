0fd064a987ccfb1b03d5b6de2e4663891e9049d5
AntonSynytsia <anton.synytsia@gmail.com>
2020-04-08 07:45:57 -0700

    Define (no code) Home, Main, Login, Register, OrderHistory components

---
+++ client/pizza-app/src/components/Login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Login = (props) => {
+  return <div></div>;
+};
+
+Login.propTypes = {};
+
+export default Login;
---
+++ client/pizza-app/src/components/Register/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Register = (props) => {
+  return <div></div>;
+};
+
+Register.propTypes = {};
+
+export default Register;
