2f4bf0956936d1dbaff0a7108da866e4eb8357eb
AntonSynytsia <anton.synytsia@gmail.com>
2020-04-08 19:08:24 -0700

    Use mysql2

--- server/src/db/dbConfig.js
+++ server/src/db/dbConfig.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize')
 
 //Change DB config for your info ('db_name', 'db_username', 'db_password')
-const sequelize = new Sequelize('PizzaStore', 'abigailrivera', 'root', {
+const sequelize = new Sequelize('PizzaStore', 'root', 'anton1', {
     host: 'localhost',
-    dialect: 'mssql',
+    dialect: 'mysql',
     dialectOptions:{
         options: {
             useUTC: false,
