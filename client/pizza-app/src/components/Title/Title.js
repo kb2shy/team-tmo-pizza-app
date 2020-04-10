0aedbbdd833a124cc28fad608e806b47ec0c53a6
AntonSynytsia <anton.synytsia@gmail.com>
2020-04-10 09:59:54 -0700

    Defined structure for app

---
+++ client/pizza-app/src/components/Title/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Title = props => {
+    return (
+        <div>
+           Title 
+        </div>
+    )
+}
+
+Title.propTypes = {
+
+}
+
+export default Title
