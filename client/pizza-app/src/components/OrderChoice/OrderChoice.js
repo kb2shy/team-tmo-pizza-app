c0c9bd3fb8cd0232137de6a67ec152949b42232a
JohnCervantes <johncamilocervantes@gmail.com>
2020-04-10 15:18:48 -0700

    Update OrderChoice.js

--- client/pizza-app/src/components/OrderChoice/OrderChoice.js
+++ client/pizza-app/src/components/OrderChoice/OrderChoice.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Title from '../Title/Title';
+import BackButton from '../BackButton/BackButton'
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import classes from './OrderChoice.module.css';
+import { setMenu } from '../../actions/menu';
 
 // OrderChoice: displayed for logged-in customer only, page 2
 // - Title sub-component
@@ -8,10 +14,32 @@ import PropTypes from 'prop-types'
 // - Simple text
 // - *BackButton component is displayed but rendered outside (don't render in component)
 const OrderChoice = props => {
+
+    const handleOrderHistory = (e) => {
+        e.preventDefault();
+        return props.setMenu(3);
+    }
+
+    const handleCreateOrder = (e) => {
+        e.preventDefault();
+        return props.setMenu(4);
+    }
+
     return (
-        <div>
-            OrderChoice
+        
+        <div className={classes.Body}>
+            <BackButton></BackButton>
+            <br></br>
+            <div className={classes.Title}> title </div>
+            <div className={classes.OrderChoice}>
+                <h1>What would you like to do today?</h1>
+                <br></br>
+                <Button onClick={handleOrderHistory}>See my order History</Button>
+
+                <Button onClick={handleCreateOrder}>Make a new order</Button>
+            </div>
         </div>
+
     )
 }
 
@@ -19,4 +47,10 @@ OrderChoice.propTypes = {
 
 }
 
-export default OrderChoice
+const mapStateToProps = (state) => ({
+    user: state.auth.user,
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setMenu })(OrderChoice);
+
