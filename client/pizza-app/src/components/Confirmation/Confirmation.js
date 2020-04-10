0ee3c2b963edd3804bf88465de7debfa8cc2c975
Chhaian Pin <chhaianpin@hotmail.com>
2020-04-10 13:27:19 -0700

    Created Confirmation Component

--- client/pizza-app/src/components/Confirmation/Confirmation.js
+++ client/pizza-app/src/components/Confirmation/Confirmation.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { setMenu } from '../../actions/menu';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 import PropTypes from 'prop-types'
 
 // Confirmation: the confirmation page
@@ -7,10 +10,45 @@ import PropTypes from 'prop-types'
 // - Create Account (CR: display for guest)
 // - The Return to home button
 const Confirmation = props => {
+    const handleClickHome = (e) => {
+        e.preventDefault();
+        return setMenu(1);
+    }
+
+    const saveOrder = () => {
+        if (!props.auth.isAuthenticated) {
+            return (
+                <Row>
+                    <Col>
+                        <p>Want to save your order?</p>
+                        <p>Create an account today!</p>
+                        <button onClick={() => props.setMenu(1)}>Create Account</button>
+                    </Col>
+                </Row>
+            )
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Container data-test="component-Confirmation" style={{ textAlign: "center"}}>
+            <Row className="col-header">
+                <Col>
+                    <h3>CONFIRMATION</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-email-message" >
+                    <Alert variant="success">Success!</Alert>
+                    <p>An email has been sent to:</p>
+                    <p>{props.auth.user.email}</p>
+                    <button onClick={(e) => this.handleClickHome(e)}>
+                        Return to Home
+                    </button>
+                </Col>
+            </Row>
+            <br />
+            {saveOrder()}
+        </Container>
     )
 }
 
@@ -18,4 +56,16 @@ Confirmation.propTypes = {
 
 }
 
-export default Confirmation
+const mapStateToProps = (state) => ({
+    // uncomment the below line and remove the dummy auth object
+    // auth: state.auth,
+    auth: { 
+        user: { 
+            email: `blahblahblah@email.com`,
+        },
+        isAuthenticated: false,
+    },
+})
+
+
+export default connect(mapStateToProps, { setMenu })(Confirmation);
