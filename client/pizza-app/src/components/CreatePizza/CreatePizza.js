cbd6e098087c12822d6e9735eb1b2a3e6fa54ade
JohnCervantes <johncamilocervantes@gmail.com>
2020-04-13 17:41:33 -0700

    Update CreatePizza.js

--- client/pizza-app/src/components/CreatePizza/CreatePizza.js
+++ client/pizza-app/src/components/CreatePizza/CreatePizza.js
@@ -1,117 +1,105 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { setBase, clearPizza } from '../../actions/pizza';
 import { addPizza } from '../../actions/pizzas';
 import { nextMenu } from '../../actions/menu';
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 import './CreatePizza.css';
 import Toppings from '../Toppings/Toppings';
 
 class CreatePizza extends React.Component {
-  //changes store to user input
-  handleChange = (e, item) => {
-    this.props.setBase(e.target.name.toLowerCase(), item);
-  };
+    constructor(props) {
+        super(props);
+        this.inputElement = React.createRef();
+    }
 
-  //Adds current pizza to pizzas array and clears current pizza
-  handleSubmit = () => {
-    const currentPizza = this.props.pizza;
-    this.props.addPizza(currentPizza);
-    this.props.clearPizza();
-    this.props.nextMenu();
-  };
+    //changes store to user input
+    handleChange = (e, item) => {
+        this.props.setBase(e.target.name.toLowerCase(), item);
+    }
 
-  //Renders topping sections
-  render() {
-    return (
-      <div className="centerDiv">
-        <h3 className="createPizzaTitle">Create Your Pizza</h3>
-        <table className="toppingTable">
-          <tbody>
-            <RadioButtonRow
-              type={'Size'}
-              options={this.props.sizes}
-              handleChange={this.handleChange}
-            />
-            <RadioButtonRow
-              type={'Crust'}
-              options={this.props.crusts}
-              handleChange={this.handleChange}
-            />
-            <RadioButtonRow
-              type={'Sauce'}
-              options={this.props.sauces}
-              handleChange={this.handleChange}
-            />
-            <RadioButtonRow
-              type={'Cheese'}
-              options={this.props.cheeses}
-              handleChange={this.handleChange}
-            />
-          </tbody>
-        </table>
+    //Adds current pizza to pizzas array and clears current pizza
+    handleSubmit = () => {
+        
+        const currentPizza = this.props.pizza;
+        this.props.addPizza(currentPizza);
+        //this.props.clearPizza();
+        this.props.nextMenu();
+    }
 
-        <table className="toppingTable">
-          <tbody>
-            <tr>
-              <td>
-                <p>Veggies</p>
-              </td>
-              <td>
-                <Toppings type={'Veggies'} />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>Meats</p>
-              </td>
-              <td>
-                <Toppings type={'Meats'} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <Button onClick={this.handleSubmit}>Add to Cart</Button>
-      </div>
-    );
-  }
+    //Renders topping sections
+    render() {
+        return (
+            <div className='centerDiv'>
+                <h3 className='createPizzaTitle'>Create Your Pizza</h3>
+                <table className='toppingTable'>
+                    <tbody>
+                        <RadioButtonRow value={this.props.pizza.size} type={'Size'} options={this.props.sizes} handleChange={this.handleChange} />
+                        <RadioButtonRow value={this.props.pizza.crust} type={'Crust'} options={this.props.crusts} handleChange={this.handleChange} />
+                        <RadioButtonRow value={this.props.pizza.sauce} type={'Sauce'} options={this.props.sauces} handleChange={this.handleChange} />
+                        <RadioButtonRow value={this.props.pizza.cheese} type={'Cheese'} options={this.props.cheeses} handleChange={this.handleChange} />
+                    </tbody>
+                </table>
+
+                <table className='toppingTable'>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>Veggies</p>
+                            </td>
+                            <td>
+                                <Toppings type={'Veggies'} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Meats</p>
+                            </td>
+                            <td>
+                                <Toppings type={'Meats'} />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <Button onClick={this.handleSubmit}>Add to Cart</Button>
+            </div>
+        )
+    }
 }
 
 //Creates a row with title and all necessary radio buttons
 class RadioButtonRow extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.type}</td>
-        {this.props.options.map((item) => {
-          return (
-            <td key={item}>
-              <input
-                type="radio"
-                name={this.props.type}
-                onChange={(e) => this.props.handleChange(e, item)}
-              />
-              <label>{item}</label>
-            </td>
-          );
-        })}
-      </tr>
-    );
-  }
+    render() {
+        return (
+            <tr>
+                <td>
+                    {this.props.type}
+                </td>
+                {this.props.options.map(item => {
+                    return (
+                        <td key={item}>
+                            <input type='radio'
+                                name={this.props.type}
+                                onChange={(e) => this.props.handleChange(e, item)}
+                                checked={item === this.props.value ? this.props.value: null}      
+                            />
+                            <label>{item}</label>
+                        </td>
+                    )
+                })}
+            </tr>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  sizes: state.database.sizes,
-  sauces: state.database.sauces,
-  cheeses: state.database.cheeses,
-  crusts: state.database.crusts,
-  pizza: state.pizza,
+    sizes: state.database.sizes,
+    sauces: state.database.sauces,
+    cheeses: state.database.cheeses,
+    crusts: state.database.crusts,
+    pizza: state.pizza,
+    newBase: state.newBase
 });
 
-export default connect(mapStateToProps, {
-  nextMenu,
-  setBase,
-  clearPizza,
-  addPizza,
-})(CreatePizza);
+export default connect(mapStateToProps, { nextMenu, setBase, clearPizza, addPizza })(CreatePizza);
