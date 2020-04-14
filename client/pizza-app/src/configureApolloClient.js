3b5873aeda29556d81275acd9c3d63117cefa136
unknown <amickey.rivera@gmail.com>
2020-04-14 14:44:51 -0700

    fixed apollo client cache issue

--- client/pizza-app/src/configureApolloClient.js
+++ client/pizza-app/src/configureApolloClient.js
@@ -1,9 +1,17 @@
 // Anton's code - use apollo-boost with all available utils
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 
+//doesn't cache queries
+const DefaultOptions = {
+  query: {
+    fetchPolicy: 'no-cache'
+  },
+}
+
 const client = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
   cache: new InMemoryCache(),
+  defaultOptions: DefaultOptions
 });
 
 export default client;
