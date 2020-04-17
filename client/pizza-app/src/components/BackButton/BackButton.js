0844a7bf3c2cc63e3291ae13ec6bc8b0d3516074
NATASHA NG <thatindependentgirl@gmail.com>
2020-04-17 15:01:58 -0700

    no back button on cart prevents user from adding another pizza by clicking back

--- client/pizza-app/src/components/BackButton/BackButton.js
+++ client/pizza-app/src/components/BackButton/BackButton.js
@@ -8,21 +8,48 @@ import StyledButton from '../common/Button/StyledButton';
 import './BackButton.css';
 
 const BackButton = ({ step, previousMenu, prevStep, setMenu, clearPizza }) => {
-  const [goToHome, setGoToHome] = useState(false);
+  /*
+    When the user clicks the "back" button on the create pizza page
+    a modal pop-up appears with options to proceed or stay on the page
+  */
   const [showAlert, setShowAlert] = useState(false);
 
   const handleCloseAlert = () => setShowAlert(false);
-  const handleShowAlert = () => {
-    setShowAlert(true);
-  };
+  const handleShowAlert = () => setShowAlert(true);
 
   const handleGoToHome = () => {
-    setGoToHome(true);
     handleCloseAlert();
     clearPizza();
     previousMenu();
-    setGoToHome(false);
   };
+
+  const renderModal = (
+    <div className="alertStyle">
+      <Modal show={showAlert} onHide={handleCloseAlert}>
+        <Modal.Header closeButton>
+          <Modal.Title>Warning! Your order is incomplete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Going to the home page will cause you to lose your pizza order. Do
+            you want to proceed?{' '}
+          </p>
+          <div className="alertStyle">
+            <StyledButton
+              variant="basicButton"
+              text="Proceed"
+              onClick={handleGoToHome}
+            />
+            <StyledButton
+              variant="basicButton"
+              text="Continue Order"
+              onClick={handleCloseAlert}
+            />
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 
   const handleClick = (evt) => {
     evt.preventDefault();
@@ -37,8 +64,8 @@ const BackButton = ({ step, previousMenu, prevStep, setMenu, clearPizza }) => {
     // [TODO]: Cart -> Edit Pizza -> clicking "back" should bring the user back to Cart page
   };
 
-  // don't display the buttons on home and confirmation pages
-  return step !== 1 && step !== 5 ? (
+  // don't display the buttons on home, cart, and confirmation pages
+  return step !== 1 && step !== 4 && step !== 5 ? (
     <div className="backButtonPositioning">
       <StyledButton
         type="button"
@@ -47,32 +74,7 @@ const BackButton = ({ step, previousMenu, prevStep, setMenu, clearPizza }) => {
         onClick={handleClick}
         text="Back"
       />
-
-      <div className="alertStyle">
-        <Modal show={showAlert} onHide={handleCloseAlert}>
-          <Modal.Header closeButton>
-            <Modal.Title>Warning! Your order is incomplete</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              Going to the home page will cause you to lose your pizza order. Do
-              you want to proceed?{' '}
-            </p>
-            <div className="alertStyle">
-              <StyledButton
-                variant="basicButton"
-                text="Proceed"
-                onClick={handleGoToHome}
-              />
-              <StyledButton
-                variant="basicButton"
-                text="Continue Order"
-                onClick={handleCloseAlert}
-              />
-            </div>
-          </Modal.Body>
-        </Modal>
-      </div>
+      {renderModal}
     </div>
   ) : (
     <Fragment />
