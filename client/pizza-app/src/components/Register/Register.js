7753233a78b4297f9e5b28e575953e1d314970cf
unknown <johncamilocervantes@gmail.com>
2020-04-22 12:50:29 -0700

    -

--- client/pizza-app/src/components/Register/Register.js
+++ client/pizza-app/src/components/Register/Register.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { registerCustomer } from '../../actions/auth';
 import { setMenu } from '../../actions/menu';
-import { Form } from 'react-bootstrap';
+import { Form, Container } from 'react-bootstrap';
 import AppSpinner from '../AppSpinner/AppSpinner';
 import StyledButton from '../common/Button/StyledButton';
 import isEmail from 'validator/lib/isEmail';
 import isAlpha from 'validator/lib/isAlpha';
 
 import StyledTitle from '../common/Title/StyledTitle';
-
+import classes from './Register.module.css'
 // Register
 // - Title component
 // - Form: first, last, email, phone, password
@@ -24,6 +24,7 @@ const Register = ({
   step,
   isAuthenticated,
   setMenu,
+  errors
 }) => {
   useEffect(() => {
     if (isAuthenticated && step === 6) {
@@ -32,21 +33,20 @@ const Register = ({
   }, [isAuthenticated, step, setMenu]);
 
   const [user, setUser] = useState({
-    firstName: guest.first_name,
-    lastName: guest.last_name,
+    first_name: guest.first_name,
+    last_name: guest.last_name,
     email: guest.email,
     password: '',
     phone: guest.phone,
   });
 
   const [touched, setTouched] = useState({
-    firstName: false,
-    lastName: false,
+    first_name: false,
+    last_name: false,
     email: false,
     password: false,
     phone: false,
   });
-
 
   function isValidPhoneNumber(phone) {
     const test1 = /^\d{10}$/;
@@ -59,8 +59,13 @@ const Register = ({
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log('hi')
-    registerCustomer({ first_name: user.firstName.trim(), last_name: user.lastName.trim(), phone: user.phone.trim(), email: user.email.trim(), password: user.password });
+    registerCustomer({
+      first_name: user.first_name.trim(),
+      last_name: user.last_name.trim(),
+      phone: user.phone.trim(),
+      email: user.email.trim(),
+      password: user.password,
+    });
   };
 
   const handleChange = (evt) => {
@@ -68,131 +73,147 @@ const Register = ({
     const value = evt.target.value;
     setUser((u) => ({ ...u, [name]: value }));
   };
-  return (
 
+  console.log(errors)
+
+  return (
     <div>
       <StyledTitle
-        divClassName="titleBox"
-        text="Create An Account "
-        className="orderChoiceTitle"
+        divClassName="titleBoxRegister"
+        text="Create an Account "
+        className="registerTitle"
       />
 
-
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            name="firstName"
-            type="text"
-            placeholder="First Name"
-            value={user.firstName}
-            onChange={handleChange}
-            isInvalid={touched.firstName && !isAlpha(user.firstName)}
-            isValid={isAlpha(user.firstName)}
-            onBlur={() => { setTouched({ firstName: true }) }}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid first name.
-        </Form.Control.Feedback>
-          <Form.Control.Feedback>
-            Looks good!
-        </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            name="lastName"
-            type="text"
-            placeholder="Last Name"
-            value={user.lastName}
-            onChange={handleChange}
-            isInvalid={touched.lastName && !isAlpha(user.lastName)}
-            isValid={isAlpha(user.lastName)}
-            onBlur={() => { setTouched({ lastName: true }) }}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid last name.
-        </Form.Control.Feedback>
-          <Form.Control.Feedback>
-            Looks good!
-        </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formPhone">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control
-            name="phone"
-            type="text"
-            placeholder="Phone"
-            value={user.phone}
-            onChange={handleChange}
-            isInvalid={touched.phone && !isValidPhoneNumber(user.phone)}
-            isValid={isValidPhoneNumber(user.phone)}
-            onBlur={() => { setTouched({ phone: true }) }}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid phone number.
-        </Form.Control.Feedback>
-          <Form.Control.Feedback>
-            Looks good!
-        </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            name="email"
-            type="email"
-            placeholder="Enter email"
-            value={user.email}
-            onChange={handleChange}
-            isInvalid={touched.email && !isEmail(user.email)}
-            isValid={isEmail(user.email)}
-            onBlur={() => { setTouched({ email: true }) }}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid email address.
-        </Form.Control.Feedback>
-          <Form.Control.Feedback>
-            Looks good!
-        </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={user.password}
-            onChange={handleChange}
-            isInvalid={touched.password && user.password === ''}
-            isValid={user.password !== ''}
-            onBlur={() => { setTouched({ password: true }) }}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a password.
-        </Form.Control.Feedback>
-          <Form.Control.Feedback>
-            Looks good!
-        </Form.Control.Feedback>
-        </Form.Group>
-        <div className="d-flex align-items-center">
-          {/* <Button variant="primary" type="submit" disabled={!isValid}>
+      <Container fluid="md" className={classes.registerFormContainer}>
+        <Form onSubmit={handleSubmit} className={classes.registerForm}>
+          <Form.Group controlId="formFirstName" >
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              className={classes.registerFormInput}
+              name="first_name"
+              type="text"
+              placeholder="First Name"
+              value={user.first_name}
+              onChange={handleChange}
+              isInvalid={touched.first_name && !isAlpha(user.first_name)}
+              isValid={isAlpha(user.first_name)}
+              onBlur={() => {
+                setTouched({ first_name: true });
+              }}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid first name.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              className={classes.registerFormInput}
+              name="last_name"
+              type="text"
+              placeholder="Last Name"
+              value={user.last_name}
+              onChange={handleChange}
+              isInvalid={touched.last_name && !isAlpha(user.last_name)}
+              isValid={isAlpha(user.last_name)}
+              onBlur={() => {
+                setTouched({ last_name: true });
+              }}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid last name.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="formPhone">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              className={classes.registerFormInput}
+              name="phone"
+              type="text"
+              placeholder="Phone"
+              value={user.phone}
+              onChange={handleChange}
+              isInvalid={touched.phone && !isValidPhoneNumber(user.phone)}
+              isValid={isValidPhoneNumber(user.phone)}
+              onBlur={() => {
+                setTouched({ phone: true });
+              }}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid phone number.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              className={classes.registerFormInput}
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              value={user.email}
+              onChange={handleChange}
+              isInvalid={touched.email && !isEmail(user.email) && errors !== ''}
+              isValid={isEmail(user.email) && errors === ''}
+              onBlur={() => {
+                setTouched({ email: true });
+              }}
+              required
+            />
+            {errors !== '' ? <Form.Control.Feedback type="invalid">
+              {errors}
+            </Form.Control.Feedback> :
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid email address.
+            </Form.Control.Feedback>}
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              className={classes.registerFormInput}
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={handleChange}
+              isInvalid={touched.password && user.password === ''}
+              isValid={user.password !== ''}
+              onBlur={() => {
+                setTouched({ password: true });
+              }}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a password.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <div className="d-flex align-items-center">
+            {/* <Button variant="primary" type="submit" disabled={!isValid}>
             Sign Up
           </Button> */}
-          <StyledButton
-            type="submit"
-            text="Sign Up"
-            variant="basicButton"
-            disabled={!isEmail(user.email) || !isAlpha(user.lastName) || !isAlpha(user.firstName) ||  user.password === '' || !isValidPhoneNumber(user.phone)}
-            text="Sign Up"
-          />
-          {loading && <AppSpinner />}
-        </div>
-      </Form>
+            <StyledButton
+              type="submit"
+              variant="basicButton"
+              disabled={
+                !isEmail(user.email) ||
+                !isAlpha(user.last_name) ||
+                !isAlpha(user.first_name) ||
+                user.password === '' ||
+                !isValidPhoneNumber(user.phone)
+              }
+              text="Sign Up"
+            />
+            {loading && <AppSpinner />}
+          </div>
+        </Form>
+      </Container>
     </div>
   );
 };
@@ -211,6 +232,7 @@ const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   guest: state.guest,
   step: state.menu.step,
+  errors: state.auth.errors
 });
 
 export default connect(mapStateToProps, {
